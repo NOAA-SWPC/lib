4b6d5ae980ab32e4b4720663a527887fb7092458
Patrick Alken <palken@geomag2.colorado.edu>
2015-10-18 09:34:13 -0600

    fix top level Makefile.am

--- magdata/print.c
+++ magdata/print.c
@@ -19,59 +19,17 @@
 #include <errno.h>
 
 #include "common.h"
-#include "euler.h"
 #include "magdata.h"
-
-/*
-magdata_euler_apply()
-  Apply Euler angle rotations to magdata dataset
-
-Inputs: data  - magdata
-        w     - workspace
-
-Return: success or error
-
-Notes:
-1) data->B_nec is replaced with data->B_vfm vectors rotated
-with Euler angles and satellite quaternions
-*/
-
-int
-magdata_euler_apply(magdata *data, const euler_workspace *w)
-{
-  int s = 0;
-  size_t i;
-
-  for (i = 0; i < data->n; ++i)
-    {
-      double *q = &(data->q[4 * i]);
-      double B_vfm[3], B_nec[3];
-      double t = data->t[i];
-
-      B_vfm[0] = data->Bx_vfm[i];
-      B_vfm[1] = data->By_vfm[i];
-      B_vfm[2] = data->Bz_vfm[i];
-
-      s += euler_vfm2nec_t(t, q, B_vfm, B_nec, w);
-
-      data->Bx_nec[i] = B_nec[0];
-      data->By_nec[i] = B_nec[1];
-      data->Bz_nec[i] = B_nec[2];
-    }
-
-  return s;
-} /* magdata_euler_apply() */
 
 int
 main(int argc, char *argv[])
 {
   magdata *data = NULL;
-  euler_workspace *euler_p = NULL;
   char *outfile = "data.dat";
   struct timeval tv0, tv1;
   int c;
 
-  while ((c = getopt(argc, argv, "i:o:e:")) != (-1))
+  while ((c = getopt(argc, argv, "i:o:")) != (-1))
     {
       switch (c)
         {
@@ -95,11 +53,6 @@ main(int argc, char *argv[])
             outfile = optarg;
             break;
 
-          case 'e':
-            fprintf(stderr, "main: reading Euler file %s...", optarg);
-            euler_p = euler_read(optarg);
-            fprintf(stderr, "done\n");
-
           default:
             break;
         }
@@ -107,16 +60,9 @@ main(int argc, char *argv[])
 
   if (!data)
     {
-      fprintf(stderr, "Usage: %s <-i magdata_file> [-o output_file] [-e euler_file]\n",
+      fprintf(stderr, "Usage: %s <-i magdata_file> [-o output_file]\n",
               argv[0]);
       exit(1);
-    }
-
-  if (euler_p)
-    {
-      fprintf(stderr, "main: applying Euler angles to data...");
-      magdata_euler_apply(data, euler_p);
-      fprintf(stderr, "done\n");
     }
 
   fprintf(stderr, "main: writing output to %s...", outfile);
