b579f6510b0e3245fb52b59ee8bf962d964f16cc
Patrick Alken <alken@colorado.edu>
2017-04-24 14:54:59 -0600

    add files

---
+++ pca/window.h
@@ -0,0 +1,16 @@
+/*
+ * window.h
+ */
+
+#ifndef INCLUDED_window_h
+#define INCLUDED_window_h
+
+/*
+ * Prototypes
+ */
+
+int apply_ps1(gsl_vector * v);
+int apply_hamming(gsl_vector * v);
+int apply_kaiser(gsl_vector * v);
+
+#endif /* INCLUDED_window_h */
